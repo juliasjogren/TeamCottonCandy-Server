512358dd8b06320733b15616ee1f0973b787d3f6
Måns Dahlström <mans@mansdahlstrom.se>
2022-01-16 02:34:08 +0100

    Fix indentation

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require("mongodb");
 
 const {
-	SERVER_IP,
-	DB_NAME,
-	DB_USER,
-	DB_PASS,
+  SERVER_IP,
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
 } = process.env;
 
 const URI = `mongodb://${DB_USER}:${encodeURIComponent(DB_PASS)}@${SERVER_IP}:27017/${DB_NAME}?retryWrites=true&writeConcern=majority`;
@@ -12,20 +12,20 @@ const client = new MongoClient(URI);
 let db = null;
 
 async function getConnection() {
-	try {
-		console.log('Attempting to connect to ', URI)
-		await client.connect();
-		console.log("DB connected");
-		db = client.db(DB_NAME);
-	
-		return db;
-	} catch (error) {
-		console.warn(error)
-	}
+  try {
+    console.log('Attempting to connect to ', URI)
+    await client.connect();
+    console.log("DB connected");
+    db = client.db(DB_NAME);
+  
+    return db;
+  } catch (error) {
+    console.warn(error)
+  }
 }
 
 module.exports = {
-	getConnection,
-	client,
+  getConnection,
+  client,
   db,
 };
