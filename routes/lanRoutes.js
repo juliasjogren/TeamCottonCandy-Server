c6f21b5a083a8fdd1a7f5eb275cd0ea468523d3c
Poliky4 <pontusmolin@me.com>
2022-03-09 21:29:11 +0100

    test

--- routes/lanRoutes.js
+++ routes/lanRoutes.js
@@ -1,6 +1,7 @@
 const { Lan } = require("../db/models/lanModel");
 const LanQueries = require("../db/queries/lanQueries");
 
+
 async function getAllLans(req, res) {
 	const lans = await LanQueries.getAllLans();
 
